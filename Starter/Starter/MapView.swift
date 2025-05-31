066d7cfbf3fe3d98a86f2230b73c63d897f70e58
Daniel Flores <daniel.flores4930@gmail.com>
2025-05-31 01:32:35 -0400

    fix map warnings

--- Starter/Starter/MapView.swift
+++ Starter/Starter/MapView.swift
@@ -4,19 +4,30 @@ import MapKit
 /// Displays a map that follows the user's current location.
 struct MapView: View {
     @StateObject private var locationManager = LocationManager()
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
-        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194),
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        )
     )
 
     var body: some View {
-        Map(coordinateRegion: $region, showsUserLocation: true)
-            .edgesIgnoringSafeArea(.all)
-            .onReceive(locationManager.$location) { location in
-                if let location {
-                    region.center = location
+        Map(position: $position) {
+            UserAnnotation()
+        }
+        .edgesIgnoringSafeArea(.all)
+        .onReceive(locationManager.$location) { location in
+            if let location {
+                withAnimation {
+                    position = .region(
+                        MKCoordinateRegion(
+                            center: location,
+                            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                        )
+                    )
                 }
             }
+        }
     }
 }
 
